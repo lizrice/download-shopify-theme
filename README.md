eab75d2c723f5085873f0ee916c1f49db4e0f4ce
Liz Rice <liz@lizrice.com>
2015-07-14 12:22:39 +0100

    Support other API calls

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # download-shopify-theme
-Command line tool to download Liquid files from a Shopify store's theme
+Command line tool to download Liquid files from a Shopify store's theme.  Now also lets you issue any API call.
 
 Run `./python theme_download.py --help` for instructions
--- theme_download.py
+++ theme_download.py
@@ -18,13 +18,15 @@ def get_shopify_theme(store, headers):
     for tt in themes: 
         if tt['role'] == 'main':
             theme_id = tt['id']
-            return theme_id
+            theme_name = tt['name']
+            return theme_id, theme_name
 
     print("No theme ID found")
-    return None
+    return None, None
 
 def get_shopify_liquid_assets(store, headers):
-    theme_id = get_shopify_theme(store, headers)
+    theme_id, theme_name = get_shopify_theme(store, headers)
+    print('Theme name: {0}'.format(theme_name))
 
     url = "https://{0}/admin/themes/{1}/assets.json".format(store, theme_id)
     r = requests.get(url, headers=headers)
@@ -39,7 +41,7 @@ def get_shopify_liquid_assets(store, headers):
 
 def get_shopify_asset(store, headers, asset):
     assert(asset)
-    theme_id = get_shopify_theme(store, headers)
+    theme_id, _ = get_shopify_theme(store, headers)
     if not theme_id:
         return None
 
@@ -51,11 +53,21 @@ def get_shopify_asset(store, headers, asset):
     liquid = asset['value'].encode("utf")
     print(liquid)
 
+def get_shopify_raw(store, headers, raw):
+    assert(raw)
+    url = "https://{0}/admin/{1}".format(store, raw)
+    r = requests.get(url, headers=headers)
+    j = r.json()
+
+    pp.pprint(j)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="By default, downloads a list of Liquid file assets in a store's main theme.  If a particular asset is specified, download that file.")
     parser.add_argument("store", help="Store domain e.g. a_shop.myshopify.com")
     parser.add_argument("token", help="Token allowing us access")
     parser.add_argument("--asset", help="Name of theme asset e.g. 'layout/theme.liquid'")
+    parser.add_argument("--raw", help="Raw URL of API call to make e.g. themes.json")
 
     args = parser.parse_args()
 
@@ -68,7 +80,9 @@ if __name__ == '__main__':
         raise SystemExit
 
     headers = shopify_access_header(args.token)
-    if args.asset:
+    if args.raw:
+        get_shopify_raw(args.store, headers, args.raw)
+    elif args.asset:
         get_shopify_asset(args.store, headers, args.asset)
     else:
         get_shopify_liquid_assets(args.store, headers)
